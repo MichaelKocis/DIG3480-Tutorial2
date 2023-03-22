912128815189b90d71ad2e7d2f7765de24cb3a79
MichaelKocis <michaelkocis@knights.ucf.edu>
2023-03-21 23:54:46 -0400

    Challenge 2

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,15 +9,98 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public Text score;
     private int scoreValue = 0;
+    public Text lives;
+    private int livesValue = 3;
+    private int levelNum;
+
+    public AudioClip musicOne;
+    public AudioClip winSound;
+    public AudioSource musicSource;
+
+    public Text winMessage;
+    public Text loseMessage;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        SetCountText();
+        levelNum = 1;
+
+        winMessage.text = " ";
+        loseMessage.text = " ";
+
+        musicSource.clip = musicOne;
+        musicSource.Play();
+
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    void SetCountText()
+    {
+        score.text = "Score: " + scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
+
+        // Teleports to Level 2
+        if ((scoreValue == 4) && (levelNum == 1))
+        {
+            transform.position = new Vector2(96.0f, 0.0f);
+            livesValue = 3;
+            lives.text = "Lives: " + livesValue.ToString();
+            levelNum = 2;
+        }
+
+        if (scoreValue == 8)
+        {
+            winMessage.text = "You Win! Game created by Michael Kocis!";
+            musicSource.clip = winSound;
+            musicSource.loop = false;
+            musicSource.Play();
+            Destroy(this);
+        }
+
+        if (livesValue <= 0)
+        {
+            loseMessage.text = "You Lose! Try again!";
+            Destroy(this);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+    }
+
     void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
@@ -31,7 +114,14 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            SetCountText();
+            Destroy(collision.collider.gameObject);
+        }
+
+        else if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            SetCountText();
             Destroy(collision.collider.gameObject);
         }
     }
@@ -42,7 +132,7 @@ public class PlayerScript : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, 4), ForceMode2D.Impulse);
             }
         }
     }
